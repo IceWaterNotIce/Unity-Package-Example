a8cc76ccb6f9ed366635d373ddac3b1310b46d61
冰水不冰 <icewaternotice@gmail.com>
2025-06-13 03:01:10 +0800

    重構 PackageExample 類別，添加 XML 註解以增強可讀性

--- Assets/PackageExample/Runtime/Scripts/PackageExample.cs
+++ Assets/PackageExample/Runtime/Scripts/PackageExample.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
-public class PackageExample : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace PackageExample
+{
+    /// <summary>
+    /// This is a simple example MonoBehaviour that can be used to demonstrate package functionality.
+    /// </summary>
+    /// <remarks>
+    /// It is placed in the Runtime folder to ensure it is included in the build.
+    /// </remarks>
+    public class PackageExample : MonoBehaviour
     {
-        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
+
